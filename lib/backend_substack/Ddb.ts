1cb53f88372fc189425d82978b357e235126ba4b
zjk97 <zhangjake97@gmail.com>
2021-11-13 23:19:38 -0500

    added preliminary setup for dynamodb

--- lib/backend_substack/Ddb.ts
+++ lib/backend_substack/Ddb.ts
@@ -1,6 +1,24 @@
+import { AttributeType, BillingMode, ProjectionType, Table } from '@aws-cdk/aws-dynamodb';
 import * as cdk from '@aws-cdk/core';
+import { RemovalPolicy } from '@aws-cdk/core';
 
 export class Ddb {
     constructor(substack: cdk.Construct, appName: string) {
+        const userTable = new Table(substack, 'prod-user-table', {
+            billingMode: BillingMode.PROVISIONED,
+            readCapacity: 20,
+            writeCapacity: 20,
+            removalPolicy: RemovalPolicy.SNAPSHOT,
+            partitionKey: {name: 'id', type: AttributeType.STRING},
+            tableName: 'userInfo'
+        });
+
+        userTable.addGlobalSecondaryIndex({
+            indexName: 'emailIndex',
+            partitionKey: {name: 'email', type: AttributeType.STRING},
+            projectionType: ProjectionType.ALL
+        });
+
+        // TODO: add other GSIs after looking through firebase before migration
     }
 }
