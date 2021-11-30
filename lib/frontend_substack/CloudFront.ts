71a4228ae39d43335f0524e47b78084aba005278
zjk97 <zhangjake97@gmail.com>
2021-11-29 23:54:24 -0500

    set up frontend infra basics, NOT yet tested

---
+++ lib/frontend_substack/CloudFront.ts
@@ -0,0 +1,44 @@
+import {Construct} from "@aws-cdk/core";
+import * as cloudfront from "@aws-cdk/aws-cloudfront";
+import * as cdk from "@aws-cdk/core";
+import * as route53 from "@aws-cdk/aws-route53";
+import * as s3 from "@aws-cdk/aws-s3";
+import {StaticSiteProps} from "./FrontendSubstack";
+
+export interface CloudFrontProps {
+    staticSiteProps: StaticSiteProps,
+    viewerCertificate: cloudfront.ViewerCertificate,
+    siteBucket: s3.Bucket,
+    cloudfrontOAI: cloudfront.OriginAccessIdentity
+}
+
+export class CloudFront{
+    constructor(substack: Construct, stackName: string, props: CloudFrontProps) {
+        const distribution = new cloudfront.CloudFrontWebDistribution(substack, `${stackName}CloudFront`, {
+            viewerCertificate: props.viewerCertificate,
+            originConfigs: [
+                {
+                    s3OriginSource: {
+                        s3BucketSource: props.siteBucket,
+                        originAccessIdentity: props.cloudfrontOAI
+                    },
+                    behaviors: [{
+                        isDefaultBehavior: true,
+                        compress: true,
+                        allowedMethods: cloudfront.CloudFrontAllowedMethods.GET_HEAD_OPTIONS,
+                    }],
+                }
+            ]
+        });
+        new cdk.CfnOutput(substack, `${stackName}DistributionId`, {value: distribution.distributionId});
+
+        // Route53 alias record for the CloudFront distribution
+        // TODO: uncomment and fix after transferring from godaddy
+        // TODO: before transferring, manually create record
+        // new route53.ARecord(substack, `${stackName}SiteAliasRecord`, {
+        //     recordName: siteDomain,
+        //     target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
+        //     zone
+        // });
+    }
+}
