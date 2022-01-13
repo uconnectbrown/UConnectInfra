6b793d904d76c3fe34430514f8066d9b03ea5878
zjk97 <zhangjake97@gmail.com>
2022-01-12 19:03:18 -0500

    updated domain and certs

--- backend/lib/Eb.ts
+++ backend/lib/Eb.ts
@@ -6,140 +6,139 @@ import iam = require('@aws-cdk/aws-iam');
 export class Eb {
     constructor(substack: cdk.Construct, appName: string) {
         const app = new elasticbeanstalk.CfnApplication(substack, `${appName}Application`, {
-          applicationName: appName,
+            applicationName: appName,
         });
-    
+
         // Create role and instance profile
         const ebRole = new iam.Role(substack, `${appName}-prod-elasticbeanstalk-ec2-role`, {
-          assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+            assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
         });
-    
+
         const ebWebPolicy = iam.ManagedPolicy.fromAwsManagedPolicyName('AWSElasticBeanstalkWebTier')
         const s3Policy = iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3FullAccess')
         const ddbPolicy = iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonDynamoDBFullAccess')
         ebRole.addManagedPolicy(ebWebPolicy);
         ebRole.addManagedPolicy(s3Policy);
         ebRole.addManagedPolicy(ddbPolicy);
-    
+
         const ebProfileName = `${appName}-InstanceProfile-prod`
-    
+
         const instanceProfile = new iam.CfnInstanceProfile(substack, ebProfileName, {
-          instanceProfileName: ebProfileName,
-          roles: [
-              ebRole.roleName
-          ]
+            instanceProfileName: ebProfileName,
+            roles: [
+                ebRole.roleName
+            ]
         });
 
-        const jwtSecretProd = secretsmanager.Secret.fromSecretNameV2(substack, `${appName}-JWT-secret-prod`, 
-        'jwt-prod');
-    
+        const jwtSecretProd = secretsmanager.Secret.fromSecretNameV2(substack, `${appName}-JWT-secret-prod`,
+            'jwt-prod');
+
         const optionSettingProperties: elasticbeanstalk.CfnEnvironment.OptionSettingProperty[] = [
-          // ----------------
-          // --auto scaling--
-          // ----------------
-          {
-              namespace: 'aws:autoscaling:launchconfiguration',
-              optionName: 'IamInstanceProfile',
-              value: ebProfileName,
-          },
-          {
-              namespace: 'aws:autoscaling:asg',
-              optionName: 'MinSize',
-              value: '1',
-          },
-          {
-              namespace: 'aws:autoscaling:asg',
-              optionName: 'MaxSize',
-              value: '1',
-          },
+            // ----------------
+            // --auto scaling--
+            // ----------------
+            {
+                namespace: 'aws:autoscaling:launchconfiguration',
+                optionName: 'IamInstanceProfile',
+                value: ebProfileName,
+            },
+            {
+                namespace: 'aws:autoscaling:asg',
+                optionName: 'MinSize',
+                value: '1',
+            },
+            {
+                namespace: 'aws:autoscaling:asg',
+                optionName: 'MaxSize',
+                value: '1',
+            },
 
-          // -----------------------
-          // --instance attributes--
-          // -----------------------
-          {
-              namespace: 'aws:ec2:instances',
-              optionName: 'InstanceTypes',
-              value: 't2.micro',
-          },
-          {
-              namespace: 'aws:elasticbeanstalk:environment',
-              optionName: 'EnvironmentType',
-              value: 'LoadBalanced',
-          },
-          {
-              namespace: 'aws:elasticbeanstalk:environment',
-              optionName: 'LoadBalancerType',
-              value: 'classic',
-          },
-          {
-              namespace: 'aws:elasticbeanstalk:cloudwatch:logs',
-              optionName: 'StreamLogs',
-              value: 'true'
-          },
-          {
-              namespace: 'aws:elasticbeanstalk:cloudwatch:logs',
-              optionName: 'RetentionInDays',
-              value: '30'
-          },
+            // -----------------------
+            // --instance attributes--
+            // -----------------------
+            {
+                namespace: 'aws:ec2:instances',
+                optionName: 'InstanceTypes',
+                value: 't2.micro',
+            },
+            {
+                namespace: 'aws:elasticbeanstalk:environment',
+                optionName: 'EnvironmentType',
+                value: 'LoadBalanced',
+            },
+            {
+                namespace: 'aws:elasticbeanstalk:environment',
+                optionName: 'LoadBalancerType',
+                value: 'classic',
+            },
+            {
+                namespace: 'aws:elasticbeanstalk:cloudwatch:logs',
+                optionName: 'StreamLogs',
+                value: 'true'
+            },
+            {
+                namespace: 'aws:elasticbeanstalk:cloudwatch:logs',
+                optionName: 'RetentionInDays',
+                value: '30'
+            },
 
-          // -----------------------
-          // --HTTPS elb listeners--
-          // -----------------------
-          {
-              namespace: 'aws:elb:listener:80',
-              optionName: 'ListenerEnabled',
-              value: 'false',
-          },
-          {
-              namespace: 'aws:elb:listener:443',
-              optionName: 'ListenerEnabled',
-              value: 'true',
-          },
-          {
-              namespace: 'aws:elb:listener:443',
-              optionName: 'ListenerProtocol',
-              value: 'HTTPS',
-          },
-          {
-              namespace: 'aws:elb:listener:443',
-              optionName: 'InstancePort',
-              value: '80',
-          },
-          {
-              namespace: 'aws:elb:listener:443',
-              optionName: 'InstanceProtocol',
-              value: 'HTTP',
-          },
-          {
-              namespace: 'aws:elb:listener:443',
-              optionName: 'SSLCertificateId',
-              // self-signed placeholder for now
-              value: 'arn:aws:acm:us-east-1:054005165999:certificate/9853d21b-80e8-484e-a97c-074116096b66',
-          },
+            // -----------------------
+            // --HTTPS elb listeners--
+            // -----------------------
+            {
+                namespace: 'aws:elb:listener:80',
+                optionName: 'ListenerEnabled',
+                value: 'false',
+            },
+            {
+                namespace: 'aws:elb:listener:443',
+                optionName: 'ListenerEnabled',
+                value: 'true',
+            },
+            {
+                namespace: 'aws:elb:listener:443',
+                optionName: 'ListenerProtocol',
+                value: 'HTTPS',
+            },
+            {
+                namespace: 'aws:elb:listener:443',
+                optionName: 'InstancePort',
+                value: '80',
+            },
+            {
+                namespace: 'aws:elb:listener:443',
+                optionName: 'InstanceProtocol',
+                value: 'HTTP',
+            },
+            {
+                namespace: 'aws:elb:listener:443',
+                optionName: 'SSLCertificateId',
+                // *.uconnectbrown.com
+                value: 'arn:aws:acm:us-east-1:054005165999:certificate/e79514cd-3dd8-42b0-a093-fc2650b539cd',
+            },
 
-          // -------------------------
-          // --environment variables--
-          // -------------------------
-          {
-              namespace: 'aws:elasticbeanstalk:application:environment',
-              optionName: 'SPRING_PROFILES_ACTIVE',
-              value: 'prod'
-          },
-          {
-              namespace: 'aws:elasticbeanstalk:application:environment',
-              optionName: 'JWT_SECRET',
-              value: jwtSecretProd.secretValue.toString()
-          }
+            // -------------------------
+            // --environment variables--
+            // -------------------------
+            {
+                namespace: 'aws:elasticbeanstalk:application:environment',
+                optionName: 'SPRING_PROFILES_ACTIVE',
+                value: 'prod'
+            },
+            {
+                namespace: 'aws:elasticbeanstalk:application:environment',
+                optionName: 'JWT_SECRET',
+                value: jwtSecretProd.secretValue.toString()
+            }
         ];
-    
+
         // Create an Elastic Beanstalk environment for production
         const preProdEnv = new elasticbeanstalk.CfnEnvironment(substack, 'ProdEnvironment', {
-          environmentName: appName + '-prod',
-          applicationName: app.applicationName || appName,
-          solutionStackName: "64bit Amazon Linux 2 v3.2.7 running Corretto 8",
-          optionSettings: optionSettingProperties
+            applicationName: app.applicationName || appName,
+            solutionStackName: "64bit Amazon Linux 2 v3.2.10 running Corretto 8",
+            optionSettings: optionSettingProperties
         });
 
         preProdEnv.addDependsOn(app);
-      }
+    }
 }
--- frontend/lib/UConnectFrontendInfraStack.ts
+++ frontend/lib/UConnectFrontendInfraStack.ts
@@ -5,28 +5,28 @@ import {CloudFront} from "./CloudFront";
 import {CloudFrontProps} from "./CloudFront";
 
 export interface StaticSiteProps {
-  domainName: string;
-  siteSubDomain: string;
+    domainName: string;
+    siteSubDomain: string;
 }
 
 export class UConnectFrontendInfraStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    const frontendId = 'UConnectFrontend';
+        const frontendId = 'UConnectFrontend';
 
-    // TODO: fill out
-    const domainName = 'brownuconnect.com';
-    const siteSubDomain = '';
-    const staticSiteProps = {domainName: domainName, siteSubDomain: siteSubDomain};
+        // TODO: fill out
+        const domainName = 'uconnectbrown.com';
+        const siteSubDomain = 'preprod';
+        const staticSiteProps = {domainName: domainName, siteSubDomain: siteSubDomain};
 
-    const bucket = new Bucket(this, frontendId, staticSiteProps);
-    const viewerCertificate = new ViewerCertificate(this, frontendId, staticSiteProps);
-    const cloudFront = new CloudFront(this, frontendId, <CloudFrontProps>{
-      staticSiteProps: staticSiteProps,
-      viewerCertificate: viewerCertificate.certificate,
-      siteBucket: bucket.siteBucket,
-      cloudfrontOAI: bucket.cloudfrontOAI
-    });
-  }
+        const bucket = new Bucket(this, frontendId, staticSiteProps);
+        const viewerCertificate = new ViewerCertificate(this, frontendId, staticSiteProps);
+        const cloudFront = new CloudFront(this, frontendId, <CloudFrontProps>{
+            staticSiteProps: staticSiteProps,
+            viewerCertificate: viewerCertificate.certificate,
+            siteBucket: bucket.siteBucket,
+            cloudfrontOAI: bucket.cloudfrontOAI
+        });
+    }
 }
